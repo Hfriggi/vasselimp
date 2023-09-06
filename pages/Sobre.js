06078c17237bc9ea5b83e860631808e722057562
Henrique Friggi <91426980+Hfriggi@users.noreply.github.com>
2023-09-06 17:46:22 -0300

    att whats button

--- pages/Sobre.js
+++ pages/Sobre.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../styles/apply.module.css'
 import MyHead from '../components/MyHead'
 import NavBar from '../components/Navbar'
+import Link from 'next/link'
 
 const Apply = () => {
   return (
@@ -19,8 +20,10 @@ const Apply = () => {
                 <h1 className="text-2xl font-bold text-white text-center pb-5">Vasselimp serviços de limpeza</h1>
               </div>
           </div>
-          <button onclick="buttonHandler()" title="WhatsApp"
-        class="fixed z-90 bottom-10 right-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300">&#9993;</button>
+        <Link href="https://api.whatsapp.com/message/YFQZPI6DCBRZJ1?autoload=1&app_absent=0" passHref>
+        <button onclick="buttonHandler()" title="WhatsApp"
+        class="fixed z-90 bottom-10 right-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"> <img src={"/images/whatss.png"} /></button>
+        </Link>
         </div>
       </section>
     </>
